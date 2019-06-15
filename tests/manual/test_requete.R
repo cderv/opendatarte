7236be7bfec867ba2fa42d636d6b354fa05da351
Christophe Dervieux <christophe.dervieux@gmail.com>
2019-06-15 14:56:08 +0200

    add manual tests

--- tests/manual/test_requete.R
+++ tests/manual/test_requete.R
@@ -9,12 +9,13 @@
 # app <- oauth_app("data.rte", client_id, client_secret)
 #
 # # 3. Get OAuth credentials
+# client credential grant : https://tools.ietf.org/html/rfc6749#section-4.4
 # # DATARTE doesn't implement OAuth 2.0 standard
 # # (http://tools.ietf.org/html/rfc6750#section-2) so we extend the Token2.0
 # # ref class to implement a custom sign method.
 #
 # # mytoken <- oauth2.0_token(datarte, app, use_basic_auth = T, without_auth_req = T)
-# mytoken <- oauth2.0_token_RTE(datarte, app, use_basic_auth = T, without_auth_req = T)
+# mytoken <- oauth2.0_token_RTE(datarte, app, use_basic_auth = T, without_auth_req = T, cache = F)
 
 # TEST REQUETE
 #
@@ -23,15 +24,22 @@
 # Authentification --------------------------------------------------------
 
 devtools::dev_mode()
+# need github httr branch
+# remotes::install_github("hadley/httr#388")
 library("httr")
-# client_id <- "8d48ab4d-bff2-47dd-b2a9-ee0add477830"
-# client_secret <- "1d208734-4ec0-4bfc-9f1f-386f2ef4a822"
+library(opendatarte)
+client_id <- Sys.getenv("RTE_API_CLIENT")
+client_secret <- Sys.getenv("RTE_API_SECRET")
 # source('R/oauth_token_without_auth.r')
-datarte_auth(client_id = .state$client_id, client_secret = .state$client_secret, cache = F)
+# datarte_auth(client_id = .state$client_id, client_secret = .state$client_secret, cache = F)
+datarte_auth(client_id = client_id, client_secret = client_secret, cache = F)
 
+get_current_token(T)
+
+is_token_available()
 
 # Test --------------------------------------------------------------------
 
-RegistreAPI()
+test1 <- RegistreAPI()
 
-unavailabilityAPI(ressource = "generation_unavailabilities")
+test2 <- unavailabilityAPI(ressource = "generation_unavailabilities")
